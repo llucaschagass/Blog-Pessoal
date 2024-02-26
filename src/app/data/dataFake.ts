0dd7ef8db0edb46e59e4bb9f7db458420cf969fe
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-02-26 11:43:53 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake
+= [
+  {
+    "id":"1",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat incidunt culpa totam aperiam nam natus voluptates eum, beatae, quam expedita quo aut nesciunt in corrupti blanditiis maxime ad eaque itaque?"
+  },
+  {
+    "id":"2",
+    "photoCover": "https://tm.ibxk.com.br/2022/05/04/04181551056727.jpg?ims=1200x675",
+    "title": "Qual é a cronologia dos filmes da Marvel?",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat incidunt culpa totam aperiam nam natus voluptates eum, beatae, quam expedita quo aut nesciunt in corrupti blanditiis maxime ad eaque itaque?"
+  },
+  {
+    "id":"3",
+    "photoCover": "https://cinebuzz.uol.com.br/media/uploads/as_marvels_deadpool_3_e_mais_quais_sao_os_proximos_filmes_da_marvel.jpg",
+    "title": "'As Marvels', 'Deadpool 3' e mais: quais são os próximos filmes da Marvel?",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat incidunt culpa totam aperiam nam natus voluptates eum, beatae, quam expedita quo aut nesciunt in corrupti blanditiis maxime ad eaque itaque?"
+  },
+  {
+    "id":"4",
+    "photoCover": "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/10/11/1420548167-as-marvels-marvel-studio-x.jpg",
+    "title": "Marvel divulga os pôsteres oficiais de 'As Marvels'",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat incidunt culpa totam aperiam nam natus voluptates eum, beatae, quam expedita quo aut nesciunt in corrupti blanditiis maxime ad eaque itaque?"
+  }
+]
